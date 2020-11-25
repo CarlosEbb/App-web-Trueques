de141d42dacfef81c2fe84a18051d0b4b737907c
kewinBarboza <kewinbarboza@gmail.com>
2020-11-25 08:41:51 -0400

    slider de home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,24 @@
 
 @section('content')
     <div class="container-fluid m-0 mt-2 p-0">
-        <img class="w-100 mt-1" src="{{asset('img/banner-home.png')}}" alt="">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block w-100 mt-1" src="{{asset('img/banner-home.png')}}" alt="">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100 mt-1" src="http://placehold.jp/1440x361.png" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100 mt-1" src="http://placehold.jp/1440x361.png" alt="Third slide">
+          </div>
+        </div>
+      </div>
     </div>
     <div class="container">
       {{-- Productos recientes --}}
