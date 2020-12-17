d703279c632c1ed6f56fea93b7513ad6d70be751
CarlosEbb <32774791+CarlosEbb@users.noreply.github.com>
2020-12-17 17:31:43 -0600

    filtro de busqueda mis anuncios

--- resources/views/users/anuncios.blade.php
+++ resources/views/users/anuncios.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.admin')
 @section('content')
+    <div class="container-fluid p-0 d-flex justify-content-between">
+        <h2 class="m-0 p-0 title-content"></h2>
+        <div class="option-menu d-flex">
+            <a class="text-table btn-menu tooltips" href="{{route('selecionar-categorias')}}">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1.5em" height="1.5em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.49 2 2 6.49 2 12s4.49 10 10 10s10-4.49 10-10S17.51 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8z" fill="#626262"/><rect x="0" y="0" width="24" height="24" fill="rgba(0, 0, 0, 0)" /></svg>
+                <span class="title-icon">Crear anuncio</span>
+                <span class="tooltiptext">Crear anuncio</span>
+            </a>
+        </div>
+    </div>
+
     <div class="container" style="min-height: 65vh;">
       <div class="row">
         <div class="col-12 text-center mt-5">
@@ -8,27 +19,31 @@
         <div class="col-12 my-5">
           <div class="row">
             <div class="col-12 col-md-12 col-lg-3">
-              <form action="">
+              <form>
                 <div class="d-flex">
-                  <input type="text" class="input pl-5" placeholder="buscar por nombre de producto">
+                  <input id="buscador" type="input" class="input pl-5" placeholder="buscar por nombre de producto" onkeyup="filtrar();">
                   <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1.5em" height="1.5em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg); left: 25px; top: 12px; position: absolute;" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5A6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5S14 7.01 14 9.5S11.99 14 9.5 14z" fill="#626262"/><rect x="0" y="0" width="24" height="24" fill="rgba(0, 0, 0, 0)" /></svg>
                 </div>
               </form>
+
             </div>
             <div class="col-9 col-md-12 col-lg-9">
               <div class="d-md-flex justify-content-center align-items-baseline">
                 <p class="d-inline-block ml-5">Filtrar por: </p>
-                <button type="button" class="btn btn-outline-info mx-2 mt-2 rounded-pill btn-sm ">Ver todos (1)</button>
-                <button type="button" class="btn btn-outline-info mx-2 mt-2 rounded-pill btn-sm ">Anuncios activos (0)</button>
-                <button type="button" class="btn btn-outline-info mx-2 mt-2 rounded-pill btn-sm ">Anuncios inactivos (0)</button>
-                <button type="button" class="btn btn-outline-info mx-2 mt-2 rounded-pill btn-sm ">Anuncios pendientes (0)</button>
+                <button type="button" class="btn btn-outline-info mx-2 mt-2 rounded-pill btn-sm " onclick="todos();">Ver todos ({{Auth::user()->productos->count()}})</button>
+                <button type="button" class="btn btn-outline-info mx-2 mt-2 rounded-pill btn-sm " onclick="todosActivos('activo');">Anuncios activos ({{Auth::user()->productos->where('status', 1)->count()}})</button>
+                <button type="button" class="btn btn-outline-info mx-2 mt-2 rounded-pill btn-sm " onclick="todosActivos('deshabilitado');">Anuncios inactivos ({{Auth::user()->productos->where('status', 0)->count()}})</button>
+                {{-- button type="button" class="btn btn-outline-info mx-2 mt-2 rounded-pill btn-sm ">Anuncios pendientes (0)</button> --}}
               </div>
             </div>
           </div>
-        </div>
+        </div> 
 
         @forelse(Auth::user()->productos as $producto)
-          <div class="col-12 mb-3">
+        
+          <div class="col-12 mb-3 item">
+          <label hidden class="nombres">{{$producto->nombre}}</label>
+          <label hidden class="estado">@if($producto->status == true) activo @else deshabilitado @endif </label>
             <div class="card shadow border-0 mb-3">
               <div class="row no-gutters">
                 <div class="col-md-2">
@@ -128,4 +143,50 @@
         @endforelse   
       </div>
     </div>
+    
+@endsection
+
+@section('scriptJS')
+
+  <script>
+      function filtrar(){
+          var nombres = $('.nombres');
+          var buscando =  $('#buscador').val();
+          var item='';
+          let count = nombres.length;
+          for( var i = 0; i < nombres.length; i++ ){
+              item = $(nombres[i]).html().toLowerCase();
+                for(var x = 0; x < item.length; x++ ){
+                    if( buscando.length == 0 || item.indexOf( buscando ) > -1 ){
+                        $(nombres[i]).parents('.item').show(); 
+                    }else{
+                        $(nombres[i]).parents('.item').hide();
+                    }
+                }
+          }
+      }
+
+      function todos(){
+        $('#buscador').val('');
+        filtrar();
+      }
+
+      function todosActivos(estado){
+          var nombres = $('.estado');
+          var buscando = estado;
+          var item='';
+          let count = nombres.length;
+          for( var i = 0; i < nombres.length; i++ ){
+              item = $(nombres[i]).html().toLowerCase();
+                for(var x = 0; x < item.length; x++ ){
+                    if( buscando.length == 0 || item.indexOf( buscando ) > -1 ){
+                        $(nombres[i]).parents('.item').show(); 
+                    }else{
+                        $(nombres[i]).parents('.item').hide();
+                    }
+                }
+          }
+      }
+  </script>
+
 @endsection
