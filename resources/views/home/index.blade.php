e0ccf31ff81eecb16be249095884b72deb9e27ae
kewinBarboza <kewinbarboza@gmail.com>
2021-01-28 21:36:22 -0400

    chat bandeja de entrada

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,7 +8,83 @@
                 <div class="col-12 text-center my-4">
                     <h2 class="title">Chat</h2>
                 </div>
-                <div class="col-md-12">
+                <div class="col-md-4">
+                    <div class="card card-border-radius content-chat" >
+                        <a href="">
+                            <div class="card-body d-flex align-items-center border-bottom p-3">
+                                <img src="{{asset('img/avatar.png')}}" class="rounded-circle"  width="50" alt="">
+                                <div class="content-info ml-3 ">
+                                    <p class="mb-0"><b>Pedro perez</b></p>
+                                    <p class="small mb-0">Lorem ipsum dolor sit amet.</p>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="">
+                            <div class="card-body d-flex align-items-center border-bottom p-3">
+                                <img src="{{asset('img/avatar.png')}}" class="rounded-circle"  width="50" alt="">
+                                <div class="content-info ml-3 ">
+                                    <p class="mb-0"><b>Pedro perez</b></p>
+                                    <p class="small mb-0">Lorem ipsum dolor sit amet.</p>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="">
+                            <div class="card-body d-flex align-items-center border-bottom p-3">
+                                <img src="{{asset('img/avatar.png')}}" class="rounded-circle"  width="50" alt="">
+                                <div class="content-info ml-3 ">
+                                    <p class="mb-0"><b>Pedro perez</b></p>
+                                    <p class="small mb-0">Lorem ipsum dolor sit amet.</p>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="">
+                            <div class="card-body d-flex align-items-center border-bottom p-3">
+                                <img src="{{asset('img/avatar.png')}}" class="rounded-circle"  width="50" alt="">
+                                <div class="content-info ml-3 ">
+                                    <p class="mb-0"><b>Pedro perez</b></p>
+                                    <p class="small mb-0">Lorem ipsum dolor sit amet.</p>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="">
+                            <div class="card-body d-flex align-items-center border-bottom p-3">
+                                <img src="{{asset('img/avatar.png')}}" class="rounded-circle"  width="50" alt="">
+                                <div class="content-info ml-3 ">
+                                    <p class="mb-0"><b>Pedro perez</b></p>
+                                    <p class="small mb-0">Lorem ipsum dolor sit amet.</p>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="">
+                            <div class="card-body d-flex align-items-center border-bottom p-3">
+                                <img src="{{asset('img/avatar.png')}}" class="rounded-circle"  width="50" alt="">
+                                <div class="content-info ml-3 ">
+                                    <p class="mb-0"><b>Pedro perez</b></p>
+                                    <p class="small mb-0">Lorem ipsum dolor sit amet.</p>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="">
+                            <div class="card-body d-flex align-items-center border-bottom p-3">
+                                <img src="{{asset('img/avatar.png')}}" class="rounded-circle"  width="50" alt="">
+                                <div class="content-info ml-3 ">
+                                    <p class="mb-0"><b>Pedro perez</b></p>
+                                    <p class="small mb-0">Lorem ipsum dolor sit amet.</p>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="">
+                            <div class="card-body d-flex align-items-center border-bottom p-3">
+                                <img src="{{asset('img/avatar.png')}}" class="rounded-circle"  width="50" alt="">
+                                <div class="content-info ml-3 ">
+                                    <p class="mb-0"><b>Pedro perez</b></p>
+                                    <p class="small mb-0">Lorem ipsum dolor sit amet.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-8">
                     <div class="card card-border-radius">
                         <div class="card-body px-0 pt-0">
                             @livewire("chat-list")
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -76,8 +76,11 @@
         <div class="dropdown">
           <a class="dropdown-toggle btn-rounded mx-1 btn-menu-notificaciones tooltips" href="#" role="button" id="dropdownMenuNotificaciones" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1.7em" height="1.7em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M19.29 17.29L18 16v-5c0-3.07-1.64-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v.68C7.63 5.36 6 7.92 6 11v5l-1.29 1.29c-.63.63-.19 1.71.7 1.71h13.17c.9 0 1.34-1.08.71-1.71zM16 17H8v-6c0-2.48 1.51-4.5 4-4.5s4 2.02 4 4.5v6zm-4 5c1.1 0 2-.9 2-2h-4a2 2 0 0 0 2 2z" fill="#25405f"/><rect x="0" y="0" width="24" height="24" fill="rgba(0, 0, 0, 0)" /></svg>
+            <span class="badge badge-pill badge-danger badge-notificaciones">
+              {{\App\Chat::orderBy("created_at", "desc")->where('user_id', Auth::user()->id)->get()->groupBy('chat-event')->count()}}
+            </span>
             <span class="tooltiptext">notificaciones</span>
-          </a> {{--\App\Chat::orderBy("created_at", "desc")->where('user_id', Auth::user()->id)->get()->groupBy('chat-event')->count()--}}
+          </a> 
           
           <div class="dropdown-menu menu menu-navbar" aria-labelledby="dropdownMenuNotificaciones">
             <ul class="menu-notificaciones-list">
