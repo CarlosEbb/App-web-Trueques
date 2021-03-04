4c8178fd3dbb76ebe7cc5eadba41dfc36f0b2eaa
kewinBarboza <kewinbarboza@gmail.com>
2021-03-04 05:41:19 -0400

    flecha de retroseso en selecionarCategoria

--- resources/views/users/selecionarCategoria.blade.php
+++ resources/views/users/selecionarCategoria.blade.php
@@ -4,14 +4,16 @@
 <div class="container mt-5" style="min-height: 63.4vh;">
   <div class="row">
     <div class="col-12 text-center my-5">
-      <h2 class="text-uppercase title">seleccionar categoria</h2>
+      <h3 class="text-uppercase title d-flex align-items-center">
+        <a href="/"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M20 11H7.83l5.59-5.59L12 4l-8 8l8 8l1.41-1.41L7.83 13H20v-2z" fill="#25405f"/><rect x="0" y="0" width="24" height="24" fill="rgba(0, 0, 0, 0)" /></svg></a>
+        seleccionar categoria
+      </h3>
     </div>
     <div class="col-5">
       <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
           @foreach( \App\Models\Categoria::orderBy('nombre', 'ASC')->get() as $categoria)
-              <a class="nav-link" id="v-pills-home" data-toggle="pill" href="#v-pills-home-{{$categoria->id}}" role="tab" aria-controls="v-pills-home-{{$categoria->id}}" aria-selected="true">{{$categoria->nombre}}</a>
+            <a class="nav-link" id="v-pills-home" data-toggle="pill" href="#v-pills-home-{{$categoria->id}}" role="tab" aria-controls="v-pills-home-{{$categoria->id}}" aria-selected="true">{{$categoria->nombre}}</a>
           @endforeach
-          
       </div>
     </div>
     <div class="col-7 col-md-6">
