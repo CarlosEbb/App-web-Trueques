a0bdb724991230457feb1c8e1b2ba452a296d044
kewinBarboza <kewinbarboza@gmail.com>
2021-03-04 21:27:49 -0400

    close categorias

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -232,6 +232,7 @@
     
       <div class="dropdown-menu menu menu-categorias mt-2 px-1" aria-labelledby="dropdownMenuLink">
         <div class="content-categorias">
+          <a href="#" class="d-block d-md-none dropdown-toggle"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1.5em" height="1.5em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M20 11H7.83l5.59-5.59L12 4l-8 8l8 8l1.41-1.41L7.83 13H20v-2z" fill="#25405f"/><rect x="0" y="0" width="24" height="24" fill="rgba(0, 0, 0, 0)" /></svg></a>
           @foreach( \App\Models\Categoria::where('status', true)->orderBy('nombre', 'ASC')->get() as $categoria)
             <div class="item-categorias">
               <div class="mt-2">
--- resources/views/users/selecionarCategoria.blade.php
+++ resources/views/users/selecionarCategoria.blade.php
@@ -5,7 +5,7 @@
   <div class="row">
     <div class="col-12 text-center my-5">
       <h3 class="text-uppercase title d-flex align-items-center justify-content-md-center">
-        <a href="/" class="d-block d-md-none"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M20 11H7.83l5.59-5.59L12 4l-8 8l8 8l1.41-1.41L7.83 13H20v-2z" fill="#25405f"/><rect x="0" y="0" width="24" height="24" fill="rgba(0, 0, 0, 0)" /></svg></a>
+        {{-- <a href="/" class="d-block d-md-none"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M20 11H7.83l5.59-5.59L12 4l-8 8l8 8l1.41-1.41L7.83 13H20v-2z" fill="#25405f"/><rect x="0" y="0" width="24" height="24" fill="rgba(0, 0, 0, 0)" /></svg></a> --}}
         seleccionar categoria
       </h3>
     </div>
