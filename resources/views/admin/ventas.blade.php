de202782f5cc292431f1dc8f9bbfda6694150cc8
kewinBarboza <kewinbarboza@gmail.com>
2021-03-11 08:49:30 -0400

    table ventas responsive

--- resources/views/admin/ventas.blade.php
+++ resources/views/admin/ventas.blade.php
@@ -5,51 +5,43 @@
 ?>
 <div class="container-fluid p-0 d-flex justify-content-between">
     <h2 class="m-0 p-0 title-content">Ventas</h2>
-    <div class="option-menu d-flex">
-
-    </div>
 </div>
 
 <div class="panel-body mt-3">
-    <table id="myTable"  class="table table-hover display">
+    <table id="myTable"  class="table table-hover display table-responsive">
         <thead>
             <tr>
-                <th class="border-top-0 text-title-table">ID</th>
-                <th class="border-top-0 text-title-table">Usuario</th>
-                <th class="border-top-0 text-title-table">ReferenceCode</th>
-                <th class="border-top-0 text-title-table">Reference_pol</th>
-                <th class="border-top-0 text-title-table">TransactionId</th>
-                <th class="border-top-0 text-title-table">lapTransactionState</th>
-                <th class="border-top-0 text-title-table">TX_VALUE</th>
-                <th class="border-top-0 text-title-table">Currency</th>
-                <th class="border-top-0 text-title-table">buyerEmail</th>
-                <th class="border-top-0 text-title-table">Days</th>
-                <th class="border-top-0 text-title-table">Producto ID</th>
-                <th class="border-top-0 text-title-table">Nombre Producto</th>
-                
+                <th class="border-top-0 text-title-table" style="font-size: 13px">ID</th>
+                <th class="border-top-0 text-title-table" style="font-size: 13px">Usuario</th>
+                <th class="border-top-0 text-title-table" style="font-size: 13px">Producto ID</th>
+                <th class="border-top-0 text-title-table" style="font-size: 13px">Nombre Producto</th>
+                <th class="border-top-0 text-title-table" style="font-size: 13px">ReferenceCode</th>
+                <th class="border-top-0 text-title-table" style="font-size: 13px">Reference_pol</th>
+                <th class="border-top-0 text-title-table" style="font-size: 13px">TransactionId</th>
+                <th class="border-top-0 text-title-table" style="font-size: 13px">lapTransactionState</th>
+                <th class="border-top-0 text-title-table" style="font-size: 13px">TX_VALUE</th>
+                <th class="border-top-0 text-title-table" style="font-size: 13px">Currency</th>
+                <th class="border-top-0 text-title-table" style="font-size: 13px">buyerEmail</th>
+                <th class="border-top-0 text-title-table" style="font-size: 13px">Days</th>
             </tr>
         </thead>
         <tbody>
             @forelse($ventas as $venta)
                 <tr class="py-0">
-                    <td class="text-table"><span>{{$venta->id}}</span></td>
-                    <td class="text-table"><span>{{\App\Models\User::find($venta->id)->name}}</span></td>
-                    <td class="text-table"><span>{{$venta->referenceCode}}</span></td>
-                    <td class="text-table"><span>{{$venta->referenceCode}}</span></td>
-                    <td class="text-table"><span>{{$venta->transactionId}}</span></td>
-                    <td class="text-table"><span>{{$venta->lapTransactionState}}</span></td>
-                    <td class="text-table"><span>{{$venta->TX_VALUE}}</span></td>
-                    <td class="text-table"><span>{{$venta->currency}}</span></td>
-                    <td class="text-table"><span>{{$venta->buyerEmail}}</span></td>
-                    <td class="text-table"><span>{{$venta->days}}</span></td>
-                    <td class="text-table"><span>{{$venta->producto_id}}</span></td>
-                    <td class="text-table"><span>{{\App\Models\Producto::find($venta->producto_id)->nombre}}</span></td>
-                    
+                    <td class="text-table"  style="font-size: 13px"><span>{{$venta->id}}</span></td>
+                    <td class="text-table"  style="font-size: 13px"><span>{{\App\Models\User::find($venta->id)->name}}</span></td>
+                    <td class="text-table"  style="font-size: 13px"><span>{{$venta->producto_id}}</span></td>
+                    <td class="text-table"  style="font-size: 13px"><span>{{\App\Models\Producto::find($venta->producto_id)->nombre}}</span></td>
+                    <td class="text-table"  style="font-size: 13px"><span>{{$venta->referenceCode}}</span></td>
+                    <td class="text-table"  style="font-size: 13px"><span>{{$venta->referenceCode}}</span></td>
+                    <td class="text-table"  style="font-size: 13px"><span>{{$venta->transactionId}}</span></td>
+                    <td class="text-table"  style="font-size: 13px"><span>{{$venta->lapTransactionState}}</span></td>
+                    <td class="text-table"  style="font-size: 13px"><span>{{$venta->TX_VALUE}}</span></td>
+                    <td class="text-table"  style="font-size: 13px"><span>{{$venta->currency}}</span></td>
+                    <td class="text-table"  style="font-size: 13px"><span>{{$venta->buyerEmail}}</span></td>
+                    <td class="text-table"  style="font-size: 13px"><span>{{$venta->days}}</span></td>
                 </tr>
-
-              
             @empty
-              
               <?php
                 $no = true;
               ?>
